0c0ee5947816fd04a7c6e9cf0801a063e73b9120
405786MoroBenjamin <405786@tecnicatura.frc.utn.edu.ar>
2024-10-14 23:05:55 -0300

    fix routing

--- App-TPI/src/app/app.routes.ts
+++ App-TPI/src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NewUserComponent } from './new-user/new-user.component'; 
 import { ListUsersComponent } from './list-users/list-users.component';
 import { UsersUpdateUserComponent } from './users-update-user/users-update-user.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     {
