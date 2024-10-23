406b1a4188d8892eabb086e9fb74ecf15c2d4708
2w1-111111-SanchezJuanManuel <111111@tecnicatura.frc.utn.edu.ar>
2024-10-23 12:37:22 -0300

    Agregado validaciones recovery-password

--- App-TPI/src/app/users-components/utils/users-recovery-password/users-recovery-password.component.ts
+++ App-TPI/src/app/users-components/utils/users-recovery-password/users-recovery-password.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-users-recovery-password',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './users-recovery-password.component.html',
   styleUrl: './users-recovery-password.component.css'
 })
 export class UsersRecoveryPasswordComponent {
 
-  email = new FormControl('',[Validators.required]);
+  private readonly router = inject(Router);
+
+
+  recoveryForm = new FormGroup({
+    email : new FormControl('',[Validators.required])
+  })
 
   //El método debe conectarse con Notificaciones 
   //para enviar al email suministrado una contraseña o token provisorio
   sendMail() {
-
+    this.router.navigate(['login']);
   }
 
 }
